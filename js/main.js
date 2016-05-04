8ca81b6a4d0028f042ff490a11713017b0c40d79
treeplantedbystreams <davbros88@hotmail.com>
2016-05-04 12:55:36 -0500

    almost done

--- js/main.js
+++ js/main.js
@@ -1,18 +1,48 @@
- angular.module('palindroneApp', ["ngRoute","ngResource","palindoneApp.controllers","palindroneApp.services"])
-.config(['$routeProvider', function($routeProvider) {
+var app= angular.module('palindroneApp', ["ngRoute"]);
+app.config(['$routeProvider', function($routeProvider) {
     $routeProvider   
- .when('/', {
+ .when('/write', {
     templateUrl: 'views/write.html',
-    controller: "listController",
+    controller: "mainController",
  })
   
- .when('/write',{
-    templateUrl : 'views/write.html',
-    controller: "reverseController"
+ .when('/read',{
+    templateUrl : 'views/read.html',
+    controller: "palindroneController",
+
  })
     
- .when('/read', {
-     templateUrl : 'views/read.html',
-     
+ .when('/', {
+         templateUrl: 'views/home.html',
  })
 }]);
+
+
+app.controller('mainController', ['$scope','$location','$rootScope', function($scope,$location,$rootScope){ 
+        $scope.addPost=function(postText){  
+            var postText= $scope.postText;
+            var words = postText.split(" ");
+            var output = new Array(); 
+            words.forEach(function(word) {
+                $rootScope.$broadcast(output.push(word.split("").reverse("").join("")));
+                }); 
+            
+            if (postText == null|| postText == ''){
+                alert('You need to enter a word.');
+            } 
+            else{
+                console.log(output); 
+                $location.path('/read');
+            };             
+        }; 
+}]);
+app.controller('palindroneController', ['$scope','$location', function($scope,$location){ 
+            $scope.$on("output", function(event,addPost){  
+            $scope.output = $scope.addPost(output);
+            }); 
+}]);
+
+
+  
+
+  
